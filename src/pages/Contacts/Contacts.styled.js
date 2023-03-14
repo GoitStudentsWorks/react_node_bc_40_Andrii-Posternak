5bf4655d9e4c2bedc86e39cf1723a5bee455574a
Andrii-Posternak <panposternak@gmail.com>
2023-03-14 22:36:56 +0200

    updated mobile width

--- src/pages/Contacts/Contacts.styled.js
+++ src/pages/Contacts/Contacts.styled.js
@@ -4,7 +4,7 @@ export const ContainerContacts = styled.div`
   font-size: 20px;
   display: flex;
   flex-direction: column;
-  min-width: 380px;
+  min-width: 340px;
   padding: 20px;
   margin-left: auto;
   margin-right: auto;
