ef6de7e1b2a5ebfbd77ba9857d2df87131e4887c
MSR333UA <matviivserhii@gmail.com>
2023-04-08 20:15:40 +0300

    add foodReducer

---
+++ src/redux/food/foodReducer.js
@@ -0,0 +1,49 @@
+import { combineReducers } from 'redux';
+import { createReducer } from '@reduxjs/toolkit';
+import foodActions from './foodActions';
+
+const {
+  fetchFoodRequest,
+  fetchFoodSuccess,
+  fetchFoodError,
+  changeFilter,
+  fetchFoodByQueryRequest,
+  fetchFoodByQuerySuccess,
+  fetchFoodByQueryError,
+  cleanState,
+} = foodActions;
+
+const food = createReducer([], {
+  [fetchFoodSuccess]: (_, { payload }) => payload,
+  [fetchFoodByQuerySuccess]: (_, { payload }) => payload,
+  [cleanState]: (state, { payload }) => {
+    return (state = []);
+  },
+});
+
+const foodFilter = createReducer('', {
+  [changeFilter]: (_, { payload }) => payload,
+});
+
+const loading = createReducer(false, {
+  [fetchFoodRequest]: () => true,
+  [fetchFoodSuccess]: () => false,
+  [fetchFoodError]: () => false,
+  [fetchFoodByQueryRequest]: () => true,
+  [fetchFoodByQueryError]: () => false,
+  [fetchFoodByQuerySuccess]: () => false,
+});
+
+const setError = (_, { payload }) => payload;
+
+const error = createReducer(null, {
+  [fetchFoodError]: setError,
+  [fetchFoodError]: setError,
+});
+
+export default combineReducers({
+  food,
+  foodFilter,
+  loading,
+  error,
+});
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,13 +1,33 @@
 const getIfLoggedIn = state => state.user.loggedIn;
-
 const getUser = state => state.user.user;
-
 const getUserId = state => state.user.user._id;
-
 const getUserNotRecommendedFood = state => state.user.user.notRecommended;
 const getUserDailyLimit = state => state.user.user.dailyLimit;
-
 const getPublicUser = state => state.user.publicUser;
+
+const getLoadingFood = state => state.food.loading;
+const getFoodFilter = state => state.food.foodFilter;
+const getFood = state => state.food.food;
+
+const getMealsId = state => state.meals.meals._id;
+const getMealsDate = state => state.meals.meals.onDay;
+const getFoodMeals = state => state.meals.meals.food;
+const getLoadingMeals = state => state.meals.loading;
+const getDate = state => state.meals.setDate;
+
+const mealsSelectors = {
+  getMealsId,
+  getFoodMeals,
+  getLoadingMeals,
+  getDate,
+  getMealsDate,
+};
+
+const foodSelectors = {
+  getLoadingFood,
+  getFoodFilter,
+  getFood,
+};
 
 const userSelectors = {
   getIfLoggedIn,
@@ -18,4 +38,4 @@ const userSelectors = {
   getUserDailyLimit,
 };
 
-export default userSelectors;
+export { userSelectors, foodSelectors, mealsSelectors };
