715e2f365a8cd94d8a269329e13446487b722411
MSR333UA <matviivserhii@gmail.com>
2023-04-11 00:31:53 +0300

    add basic html

--- src/components/DailyCaloriesForm/DailyCaloriesForm.jsx
+++ src/components/DailyCaloriesForm/DailyCaloriesForm.jsx
@@ -1,9 +1,65 @@
-import { DailyCalorieIntake } from 'components/DailyCalorieIntake/DailyCalorieIntake';
+// import { DailyCalorieIntake } from 'components/DailyCalorieIntake/DailyCalorieIntake';
+import { Formik, Form } from 'formik';
 
 export const DailyCaloriesForm = () => {
   return (
     <>
-      <DailyCalorieIntake />
+      <Formik
+        initialValues={{
+          height: '',
+          age: '',
+          weight: '',
+          desiredWeight: '',
+          bloodType: '',
+        }}
+      >
+        {() => (
+          <Form>
+            <h1>Calculate your daily calorie intake right now</h1>
+            <div>
+              <div>
+                <div>
+                  <div>
+                    <p></p>
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <p></p>
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <p></p>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <div>
+                    <p></p>
+                  </div>
+                </div>
+                <div>
+                  <h3>Blood type *</h3>
+
+                  <ul></ul>
+                  <div>
+                    <p></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <button type="submit" variant="start">
+                Start losing weight
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+
+      {/* <DailyCalorieIntake /> */}
     </>
   );
 };
