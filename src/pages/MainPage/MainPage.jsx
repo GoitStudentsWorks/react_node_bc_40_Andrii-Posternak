9adb46e6ddd51e31b49a23e2185a9deb8f200a53
MSR333UA <matviivserhii@gmail.com>
2023-04-11 03:05:32 +0300

    add img

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,5 @@
 import { DailyCaloriesForm } from 'components/DailyCaloriesForm/DailyCaloriesForm';
 
 export const MainPage = () => {
-  return (
-    <div className="container">
-      <h1>main page</h1>
-
-      <DailyCaloriesForm />
-    </div>
-  );
+  return <DailyCaloriesForm />;
 };
