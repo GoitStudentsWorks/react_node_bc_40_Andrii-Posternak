7da50f56b5986f07f8ca38eb4ffeaec5c6ba3a3e
Romaniv Dmytro <romanivdmytro@gmail.com>
2023-04-11 09:50:37 +0300

    added loginForm, RegisterForm markups and styles

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,7 +1,19 @@
+import styles from './LoginForm.module.scss';
+
 export const LoginForm = () => {
   return (
-    <div>
-      <div></div>
-    </div>
+    <>
+      <p className={styles.heading}>Log in</p>
+      <form className={styles.formWrapper}>
+        <label className={styles.label}>
+          <span className={styles.text}>Email</span>
+          <input type="email" className={styles.input} />
+        </label>
+        <label className={styles.label}>
+          <span className={styles.text}>Password</span>
+          <input type="password" className={styles.input} />
+        </label>
+      </form>
+    </>
   );
 };
--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,7 +1,23 @@
+import styles from './RegistrationForm.module.scss';
+
 export const RegistrationForm = () => {
   return (
-    <div>
-      <div></div>
-    </div>
+    <>
+      <p className={styles.heading}>Register</p>
+      <form className={styles.formWrapper}>
+        <label className={styles.label}>
+          <span className={styles.text}>Name</span>
+          <input type="text" className={styles.input} />
+        </label>
+        <label className={styles.label}>
+          <span className={styles.text}>Email</span>
+          <input type="email" className={styles.input} />
+        </label>
+        <label className={styles.label}>
+          <span className={styles.text}>Password</span>
+          <input type="password" className={styles.input} />
+        </label>
+      </form>
+    </>
   );
 };
