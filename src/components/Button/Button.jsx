80125cf67823aff94269894e70b920e99af380c3
Romaniv Dmytro <romanivdmytro@gmail.com>
2023-04-11 11:05:41 +0300

    added main button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,21 @@
-export const Button = () => {
+import clsx from 'clsx';
+
+import styles from './Button.module.scss';
+
+export const Button = ({
+  size = 'small',
+  mainStyle,
+  type,
+  children,
+  handleClick,
+}) => {
   return (
-    <div>
-      <div></div>
-    </div>
+    <button
+      type={type}
+      onClick={handleClick}
+      className={clsx(styles[size], styles[mainStyle])}
+    >
+      {children}
+    </button>
   );
 };
