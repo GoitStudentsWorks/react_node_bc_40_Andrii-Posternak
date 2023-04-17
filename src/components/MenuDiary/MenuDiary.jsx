6b85a072001d6be030dde66849c3a3d5804a5922
DemoNataliia <nataliam110388@gmail.com>
2023-04-17 13:31:41 +0300

    addad arrow

--- src/components/MenuDiary/MenuDiary.jsx
+++ src/components/MenuDiary/MenuDiary.jsx
@@ -1,21 +1,21 @@
 import PropTypes from 'prop-types';
 import React, { memo } from 'react';
 import { DiaryAddProductForm } from 'components/DiaryAddProductForm/DiaryAddProductForm';
-import svg from '../../assets/images/svg/Vector 9.svg';
 import style from './MenuDiary.module.scss';
+import { TbArrowBack } from 'react-icons/tb';
 
 export const MenuDiary = memo(({ onClick }) => {
   return (
-    <div className="container">
+    <>
       <div className={style.modal}>
         <div className={style.list}>
           <button className={style.btn} onClick={onClick}>
-            <img src={svg} alt="arrow" />
+            <TbArrowBack />
           </button>
           <DiaryAddProductForm />
         </div>
       </div>
-    </div>
+    </>
   );
 });
 
