dc9421642dd95b0c4be1731221d301dbd972a3bd
Andrii-Posternak <panposternak@gmail.com>
2023-04-17 10:32:50 +0300

    changed add product form select

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://slim-mom-backend-tf5k.onrender.com/api';
 
+// slim - mom - backend - production.up.railway.app;
+
 export const setToken = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
