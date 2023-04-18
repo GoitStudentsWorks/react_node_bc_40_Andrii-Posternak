07c7831f380117bb66783e38fbfb602bf306ca62
MSR333UA <matviivserhii@gmail.com>
2023-04-18 14:29:38 +0300

    added background image

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,12 +1,13 @@
 import { LoginForm } from 'components/LoginForm/LoginForm';
 import styles from './LoginPage.module.scss';
 
-
 export const LoginPage = () => {
   return (
-    <div className="container">
-      <div className={styles.formWrapper}>
-        <LoginForm />
+    <div className={styles.loginPage}>
+      <div className="container">
+        <div className={styles.formWrapper}>
+          <LoginForm />
+        </div>
       </div>
     </div>
   );
--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -3,9 +3,11 @@ import styles from './RegistrationPage.module.scss';
 
 export const RegistrationPage = () => {
   return (
-    <div className="container">
-      <div className={styles.formWrapper}>
-        <RegistrationForm />
+    <div className={styles.registerPage}>
+      <div className="container">
+        <div className={styles.formWrapper}>
+          <RegistrationForm />
+        </div>
       </div>
     </div>
   );
