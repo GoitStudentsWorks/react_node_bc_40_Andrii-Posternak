5137d97107040209b4db954c09458f0e57002453
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-04-20 22:15:34 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/react_node_bc_40_Andrii-Posternak@b6e3a547f20adb60318d5684d05f673074cc9f35 🚀

---
+++ static/js/53.b1d49ad5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkslim_mom_frontend=self.webpackChunkslim_mom_frontend||[]).push([[53],{6053:function(t,n,e){e.d(n,{nfZ:function(){return o}});var r=e(9983);function o(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",stroke:"#000",strokeWidth:"2",d:"M3,3 L21,21 M3,21 L21,3"}}]})(t)}}}]);
+//# sourceMappingURL=53.b1d49ad5.chunk.js.map
